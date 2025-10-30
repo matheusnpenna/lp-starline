4ccb16b3a743defa02d058c982d7961aa0a7ec54
Matheus Penna <matheusnpenna@gmail.com>
2025-10-29 23:43:04 -0300

    add plan control

--- frontend/js/plans.js
+++ frontend/js/plans.js
@@ -1,34 +1,3 @@
-const form = document.querySelector('form');
-
-if (form) {
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(form);
-    const data = {};
-    formData.forEach((value, key) => {
-      data[key] = value;
-    });
-
-    window.open(`https://wa.me/557499949898?text=${
-      encodeURIComponent(`
-        E-mail: ${data.email}
-        Nome: ${data.name}
-        WhatsApp: ${data.whastapp}
-        Assunto: ${data.subject}
-        Mensagem: ${data.message}
-      `)
-    }`)
-  }); 
-}
-const items = document.querySelectorAll('.starline-marquee__wrapper');
-const marqueeInner = document.querySelector('.starline-marquee__inner');
-
-if (marqueeInner && items.length > 0) {
-  items.forEach(item => {
-    marqueeInner.insertAdjacentElement('beforeend', item.cloneNode(true));
-  });
-}
 const plans = {
   enterprise: [
     { name: 'EMPRESARIAL 1', velocity: '100', price: { int: '94', decimal: '90' } },
@@ -94,8 +63,3 @@ plansSelectorItems.forEach(el => {
     changePlansInfo(type)
   })
 })
-const yearEl = document.querySelector('.starline-footer__divider-info__year')
-
-if (yearEl) {
-  yearEl.innerText = new Date().getFullYear();
-}
